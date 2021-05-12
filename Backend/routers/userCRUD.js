e261cef6a0f902c97991e1f544a826ca405beffa
이경호 <dlrudgh1303@gmail.com>
2021-05-12 23:29:16 +0900

    Node.js로 CRUD 구조 이해

--- Backend/routers/userCRUD.js
+++ Backend/routers/userCRUD.js
@@ -23,6 +23,52 @@ router.post('/create', async (req, res) => {
         }
 });
 
+/** 해당 user가 존재하는지 여부 확인  */
+router.get('/find', async(res, req) => {
+    try {
+        const obj = {email : req.body.email};
+        const user = await User.findOne(obj);
+        console.log(user);
+        res.json({message  : "이메일이 존재합니다. "});
+    }
+    catch (err) {
+        console.log(err);
+        res.json({message : false});
+
+    }
+});
+
+
+
+router.post("/update", async(res, req) => {
+    try{
+        await User.update({
+            name :req.body.name,
+            email : req.body.email,
+            password : req.body.password
+        });
+        res.json({message :true});
+    } catch (err) {
+        console.log(err);
+        res.json({message : false});
+    }
+});
+
+router.post("/delete", async (req, res) => {
+    try{
+        await User.remove({
+            name : req.body.name
+        });
+        res.json({message : true});
+    } catch (err) {
+        console.log(err);
+        res.json({message : false});
+
+    }
+
+})
+
+
 
 
 module.exports = router;
